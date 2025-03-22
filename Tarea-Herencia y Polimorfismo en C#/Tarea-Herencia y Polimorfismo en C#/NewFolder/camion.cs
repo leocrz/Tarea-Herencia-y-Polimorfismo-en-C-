b08c72313206d950e8363cffcb0ce1f8cc7a51b7
JOSE LEONEL CRUZ LOPEZ <jcruzl20@miumg.edu.gt>
2025-03-21 23:24:58 -0600

    Add files via upload

---
+++ Tarea-Herencia y Polimorfismo en C#/Tarea-Herencia y Polimorfismo en C#/NewFolder/camion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1bpoo.MisClases
+{
+    internal class Camion : Vehiculo
+    {
+        private int capacidadCarga; // Encapsulación
+        private int numeroEjes;     // Encapsulación
+        private double pesoMaximo;  // Encapsulación
+
+        public Camion(int anio, string elColor, string elModelo, int capacidadCarga, int numeroEjes, double pesoMaximo)
+            : base(anio, elColor, elModelo)
+        {
+            this.capacidadCarga = capacidadCarga;
+            this.numeroEjes = numeroEjes;
+            this.pesoMaximo = pesoMaximo;
+        }
+
+        public override void Acelerar(int cuanto)
+        {
+            if (encendido)
+            {
+                base.Acelerar(cuanto);
+                Console.WriteLine("El camión está acelerando.");
+            }
+            else
+            {
+                Console.WriteLine("No se puede acelerar. El camión está apagado.");
+            }
+        }
+
+        public override void Frenar()
+        {
+            if (encendido)
+            {
+                base.Frenar();
+                Console.WriteLine("El camión está frenando.");
+            }
+            else
+            {
+                Console.WriteLine("No se puede frenar. El camión está apagado.");
+            }
+        }
+
+        public override void Encender()
+        {
+            encendido = true;
+            Console.WriteLine("Camión encendido.");
+        }
+
+        public override void Apagar()
+        {
+            encendido = false;
+            Console.WriteLine("Camión apagado.");
+        }
+
+        public int ObtenerCapacidadCarga()
+        {
+            return capacidadCarga;
+        }
+
+        public int ObtenerNumeroEjes()
+        {
+            return numeroEjes;
+        }
+
+        public double ObtenerPesoMaximo()
+        {
+            return pesoMaximo;
+        }
+    }
+}
---
+++ Tarea-Herencia y Polimorfismo en C#/Tarea-Herencia y Polimorfismo en C#/NewFolder/programa princial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using p1bpoo.MisClases;
+
+namespace p1bpoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo miCarrito = new Vehiculo(2026, "Azul", "Alfa Romeo");
+            Vehiculo elOtro = new Vehiculo(2000, "Rojo", "DELOREAN");
+            CarroElectrico miBYD = new CarroElectrico(2026, "Gris", "Tesla");
+            Console.WriteLine("\tCarro Electrico");
+            miBYD.InformacionVehiculo();
+            miBYD.CargarBateria();
+            Console.WriteLine("El nivel de batería es: {0}", miBYD.NivelBateria());
+
+            // Llamadas a métodos heredados para CarroElectrico
+            miBYD.Encender();
+            miBYD.Acelerar(20);
+            miBYD.Frenar();
+            //miBYD.Apagar();
+
+            // Creación de instancia de AutoDeCombustion
+            AutoDeCombustion miAutoCombustion = new AutoDeCombustion(2025, "Negro", "Ford Mustang", 50, "Gasolina");
+            Console.WriteLine("\n");
+            Console.WriteLine("\tAutoCombustion");
+            // Llamadas a métodos de AutoDeCombustion
+            miAutoCombustion.InformacionVehiculo();
+            miAutoCombustion.Encender();
+            miAutoCombustion.Acelerar(30);
+            miAutoCombustion.Frenar();
+            //miAutoCombustion.Apagar();
+            miAutoCombustion.RellenarTanque();
+
+            // Creación de instancia de Motocicleta
+            Motocicleta miMoto = new Motocicleta(2025, "Rojo", "Ducati", 1000, "Deportiva", 80);
+            Console.WriteLine("\n");
+            Console.WriteLine("\tMotocicleta");
+            // Llamadas a métodos de Motocicleta
+            miMoto.InformacionVehiculo();
+            miMoto.Encender();
+            miMoto.Acelerar(15);
+            Console.WriteLine("Cilindrada: {0}", miMoto.ObtenerCilindrada());
+            Console.WriteLine("Tipo de motocicleta: {0}", miMoto.ObtenerTipoMoto());
+            Console.WriteLine("Nivel de aceite: {0}", miMoto.ObtenerNivelAceite());
+            miMoto.Frenar();
+            //miMoto.Apagar();
+
+            // Creación de instancia de Camion
+            Camion miCamion = new Camion(2025, "Blanco", "Isuzu", 10000, 4, 20000);
+            Console.WriteLine("\n");
+            Console.WriteLine("\tcamion");
+            // Llamadas a métodos de Camion
+            miCamion.InformacionVehiculo();
+            miCamion.Encender();
+            miCamion.Acelerar(10);
+            Console.WriteLine("Capacidad de carga: {0}", miCamion.ObtenerCapacidadCarga());
+            Console.WriteLine("Número de ejes: {0}", miCamion.ObtenerNumeroEjes());
+            Console.WriteLine("Peso máximo: {0} kg", miCamion.ObtenerPesoMaximo());
+            miCamion.Frenar();
+            //miCamion.Apagar();
+        }
+    }
+}
